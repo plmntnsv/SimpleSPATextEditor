ff9c159a945c931dd2fb065d958b153549d95290
Plamen <plamen.atanasov91@gmail.com>
2017-04-27 22:48:04 +0300

    save, clear btns functionality added

--- public/scripts/controllers/document-controller.js
+++ public/scripts/controllers/document-controller.js
@@ -1,5 +1,9 @@
-import { templateLoader } from 'templateLoader';
-import { DocumentFile } from 'documentFile';
+import {
+    templateLoader
+} from 'templateLoader';
+import {
+    DocumentFile
+} from 'documentFile';
 
 let $contentContainer = $("#contents-container");
 
@@ -10,34 +14,65 @@ export function get() {
             $contentContainer.html(html);
 
             let $saveBtn = $("#save-btn");
+            let $saveFileBtn = $("#save-file-btn");
+            let $cancelBtn = $("#cancel-btn");
+            let $inputSearchVal = $("#input-search");
+            let $searchBtn = $("#search-btn");
             let $saveContainer = $(".save-window");
-            
-                console.log($saveContainer);
-                console.log($saveBtn);
             let $clearBtn = $("#clear-btn");
             let $txtArea = $("#txt-area");
             let $dumpster = $("#dumpster");
-            let val;
+            let textContent;
+            let searchContent;
 
             if ($dumpster.html() !== '') {
-                $txtArea.val($dumpster.html());
+                $txtArea.textContent($dumpster.html());
             }
 
             $txtArea.on("input", function () {
                 $dumpster.html($txtArea.val());
+                if ($txtArea.val() === '') {
+                    $saveFileBtn.attr('disabled', 'disabled');
+                    $clearBtn.attr('disabled', 'disabled');
+                } else {
+                    $saveFileBtn.removeAttr('disabled');
+                    $clearBtn.removeAttr('disabled');
+                }
+            });
+
+            $searchBtn.on("click", function () {
+                searchContent = $inputSearchVal.val();
+                let foundMatch = $txtArea.val().indexOf(searchContent);
+
+                while (foundMatch > 0) {
+                    
+
+                    foundMatch = $txtArea.val().indexOf(searchContent, foundMatch + 1);
+                }
+
+            });
+
+            $saveFileBtn.on("click", function () {
+                $saveContainer.toggleClass("hidden");
+            });
+
+            $cancelBtn.on("click", function () {
+                $saveContainer.toggleClass("hidden");
             });
 
             $saveBtn.on("click", function () {
                 $saveContainer.toggleClass("hidden");
-                val = $txtArea.val();
-                let newFile = new DocumentFile("name", "author", val); 
-                console.log(newFile);               
-                $dumpster.html(val);
+                textContent = $txtArea.val();
+                let newFile = new DocumentFile("name", "author", textContent);
+                console.log(newFile);
+                $dumpster.html(textContent);
             });
 
             $clearBtn.on("click", function () {
-               $txtArea.val('');
-               $dumpster.html('');
+                $saveFileBtn.attr('disabled', 'disabled');
+                $clearBtn.attr('disabled', 'disabled');
+                $txtArea.val('');
+                $dumpster.html('');
             });
         });
 }
